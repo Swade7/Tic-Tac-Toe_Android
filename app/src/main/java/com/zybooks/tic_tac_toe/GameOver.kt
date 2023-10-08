e8d01a320fbbd7bb21372bd02213bd3322a063e7
Swade7 <swade7@harding.edu>
2023-10-08 17:44:43 -0500

    Add UI

---
+++ app/src/main/java/com/zybooks/tic_tac_toe/GameOver.kt
@@ -0,0 +1,11 @@
+package com.zybooks.tic_tac_toe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameOver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_over)
+    }
+}
