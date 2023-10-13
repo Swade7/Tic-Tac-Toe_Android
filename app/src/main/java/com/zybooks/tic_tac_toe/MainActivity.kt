c1b5e7db7c361dc885fa8c636b1bf2d7984e2be0
Swade7 <swade7@harding.edu>
2023-10-13 12:18:44 -0500

    Set onSaveInstancState to save a string containing the current state of the board

--- app/src/main/java/com/zybooks/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/zybooks/tic_tac_toe/MainActivity.kt
@@ -45,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBooleanArray(GAME_STATE, game.state)
+        outState.putString(GAME_STATE, game.state)
     }
 
 }
